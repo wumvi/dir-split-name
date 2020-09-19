757e0e64e3f262150792ae99717a51b1e8946171
VladKopalov <vladkop@mail.ru>
2020-09-19 20:55:05 +0300

    git init

---
+++ src/DirSplitName.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wumvi\Utils;
+
+class DirSplitName
+{
+    public static function get(array $parts): string
+    {
+        $result = '';
+        foreach ($parts as $part) {
+            $len = strlen($part . '');
+            $len = $len + $len % 2;
+            $result .= chunk_split(str_pad($part . '', $len, '0', STR_PAD_LEFT), 2, '/');
+        }
+
+        return $result;
+    }
+}
